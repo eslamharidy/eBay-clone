e4a6567fa46490808c87aae26c179ca53168f693
shuaibcs <alshuaibi7@gmail.com>
2019-11-13 09:42:02 +0100

    backend

--- adv/model.js
+++ adv/model.js
@@ -3,13 +3,13 @@ const db = require('../db')
 
 //Declare an Adv model with 6 string fields:
 
-const Adv = db.define('event',{
+const Adv = db.define('advertisement',{
     title:Sequelize.STRING,
     description:Sequelize.STRING,
     picture:Sequelize.STRING,
     price:Sequelize.STRING,
     email:Sequelize.STRING,
-    phone:Sequelize.STRING,
+    phone:Sequelize.INTEGER,
 })
 
 
